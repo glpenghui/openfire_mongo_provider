336128bab721b8fd5cf6cff1c06ece2fea184b25
huxingbang <huxingbang_09@163.com>
2013-11-26 16:28:47 +0800

    add sql options

---
+++ sql.sql
@@ -0,0 +1,4 @@
+update ofProperty set propValue="com.mmbang.im.provider.MongoAdminProvider" where name="provider.admin.className";
+update ofProperty set propValue="com.mmbang.im.provider.MongoAuthProvider" where name="provider.auth.className";
+update ofProperty set propValue="com.mmbang.im.provider.MongoUserProvider" where name="provider.user.className";
+update ofProperty set propValue="com.mmbang.im.provider.MongoVCardProvider" where name="provider.vcard.className";
